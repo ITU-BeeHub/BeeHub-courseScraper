b51c24188410ee9059b4be5af29626f66bbbdc80
MustafaKrc <mustafa.kirci0@gmail.com>
2024-07-17 18:12:37 +0300

    Converted course file format to json

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import requests
 import datetime
 import json
+from copy import deepcopy
 
 from github import pushChanges
 from bs4 import BeautifulSoup
@@ -12,21 +13,31 @@ date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 repo_root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-def fetchCoursePage(course_code : str) -> list[str]:
+def fetchCoursePage(course_code: str) -> list[dict[str:str]]:
     url = f"{base_url}&derskodu={course_code}"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # print the div with the class "table-responsive"
+    # Find the div with the class "table-responsive"
     table = soup.find("div", class_="table-responsive")
     
-    #convert table to csv format
-    rows = []
+    # Convert table to json format with corresponding headers
+    courses = []
+    headers = []
+    course = {}
+
     for i, row in enumerate(table.find_all("tr")):
-        if i >= 1:  # skip the first row (turkish headers)
-            rows.append(",".join([f'"{cell.get_text()}"' for cell in row.find_all(["td", "th"])]) + "\n")
-    
-    return rows
+        if i == 0: # skip Turkish headers
+            continue
+        elif i == 1: # get English headers
+            headers = [str(cell.text).strip() for cell in row.find_all(["td", "th"])]
+        else: # get course info
+            for idx, cell in enumerate(row.find_all(["td", "th"])):
+                course[headers[idx]] = str(cell.get_text()).strip()
+            courses.append(deepcopy(course))
+            course.clear()
+
+    return courses
 
 def fetchCourses() -> list[str]:
     # Send GET request to the specified URL    
@@ -51,19 +62,20 @@ if __name__ == "__main__":
     for course_code in course_codes:
 
         # Fetch the course page
-        rows = fetchCoursePage(course_code)
+        course = fetchCoursePage(course_code)
 
         # Write the course page to a file
-        file_path = os.path.join(repo_root_dir, "public", date, f"{course_code}.csv")
+        file_path = os.path.join(repo_root_dir, "public", date, f"{course_code}.json")
         with open(file_path, "w", encoding="utf-8") as file:
-            file.write("".join(rows))
+            json.dump(course, file, ensure_ascii=False)
+
 
     # Update most_recent.txt file
-    with open(os.path.join(repo_root_dir, "public", "most_recent.txt"), "w") as file:
+    with open(os.path.join(repo_root_dir, "public", "most_recent.txt"), "w", encoding="utf-8") as file:
         file.write(date)
 
     # Update the course_codes.json file
-    with open(os.path.join(repo_root_dir, "public", "course_codes.json"), "w") as file:
-        json.dump(course_codes, file)
+    with open(os.path.join(repo_root_dir, "public", "course_codes.json"), "w", encoding="utf-8") as file:
+        json.dump(course_codes, file, ensure_ascii=False)
 
     pushChanges(repo_root_dir, f"Add course schedules for {date}")
